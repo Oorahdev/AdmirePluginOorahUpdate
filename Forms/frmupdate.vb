612006bf932f4068a69663069e5a9af29922e075
lfireman <leah.fireman@oorah.org>
2019-02-15 11:18:09 -0500

    'passes in the current user together with the list of dlls to update. Fully functional, finished coding

--- Forms/frmupdate.vb
+++ Forms/frmupdate.vb
@@ -8,10 +8,10 @@ Public Class frmupdate
     Dim util As New Utilities
     Public host As admAdmireCustomModuleInterface.AdmireHostParameters
     Public Plugin As AdmireUpdate
-    ' Public Shared result As String
     Public Shared rows As DataRow()
     Public Shared stringArr
-    Public Shared labels
+    Public labels
+
 
     Public Sub New(admireHost As admAdmireCustomModuleInterface.AdmireHostParameters)
         host = admireHost
@@ -39,11 +39,13 @@ Public Class frmupdate
         host.SqlMaster.PullDataTable("UpdatedDlls", "AdmireUpdatedDLLs", arrargs, True)
         Dim labels As New StringBuilder
         rows = host.SqlMaster.DataTables("UpdatedDlls").Select("CUDLL_name is not null", "CUDLL_name")
+
         For Each row As DataRow In rows
             labels.Append(" " + row.Item("CUDLL_name"))
         Next
+        Me.labels = labels
         If host.SqlMaster.DataTables("UpdatedDlls").Rows.Count > 0 Then
-            Button1.Text = labels.ToString() '"Update Required"
+            Button1.Text = "Update Required"
             Button1.ForeColor = Color.White
             Button1.BackColor = Color.Red
         Else
@@ -57,18 +59,54 @@ Public Class frmupdate
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
-            Dim exe = "C:\Users\lfireman\Documents\GitHub\updateAdmire\AwsConsoleApp1\bin\Debug\AwsConsoleApp1.exe"
-            Dim args = labels
-            Process.Start(exe, args)
+            downloadDlls()
+            ' markUpdated()
+            ' reopenAdmire()
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
-        Dim arrargs As Object() = {host.SqlMaster.UserId}
-        host.SqlMaster.RunSql("AdmireUpdateDlls_lastUpdate", arrargs)
-        Button1.Text = "Update Successful"
-        Button1.ForeColor = Color.White
-        Button1.BackColor = Color.MediumSeaGreen
     End Sub
 
+
+    Private Sub downloadDlls()
+        Try
+            Dim exe = "C:\Users\lfireman\Documents\GitHub\updateAdmire\AwsConsoleApp1\bin\Debug\AwsConsoleApp1.exe"
+            Dim dlls = labels.ToString()
+
+            Dim arrargs As Object() = {host.SqlMaster.UserId}
+            host.SqlMaster.PullDataTable("UsrId", "Usr_UsrId", arrargs, True)
+            Dim usr As String = Convert.ToString(Nz(host.SqlMaster.DataTables("UsrId").Rows(0).Item("USR_ID"), 0))
+
+            Process.Start(exe, usr & dlls)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    'Private Sub closeAdmire()
+    '    Try
+    '        Process.Start("taskkill /IM admire.exe")
+    '    Catch ex As Exception
+    '        MsgBox(ex.Message)
+    '    End Try
+    'End Sub
+
+    'Private Sub reopenAdmire()
+    '    Try
+    '        Process.Start("start /d C:\Admire admire.exe")
+    '    Catch ex As Exception
+    '        MsgBox(ex.Message)
+    '    End Try
+    'End Sub
+
+
+
+    'Private Sub markUpdated()
+    '    Dim arrargs As Object() = {host.SqlMaster.UserId}
+    '    host.SqlMaster.RunSql("AdmireUpdateDlls_lastUpdate", arrargs)
+    '    Button1.Text = "Update Successful"
+    '    Button1.ForeColor = Color.White
+    '    Button1.BackColor = Color.MediumSeaGreen
+    'End Sub
 
 End Class
